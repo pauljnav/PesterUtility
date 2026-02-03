30a69d6d43c725c998d436fde2d43debdf91c628
pauljnav - Paul Naughton <tecknikp@gmail.com>
2026-02-03 21:10:53 +0000

    Update function and Tests when expanding the AST detail to include Name, IsFilter, Parameters and more

--- Get-FunctionName.Tests.ps1
+++ Get-FunctionName.Tests.ps1
@@ -16,8 +16,8 @@ Describe "Get-FunctionName Tests" {
         $tempPath = "TestDrive:\TestScript.ps1"
         Set-Content $tempPath -Value @'
             function Test-One { }
-            function Test-Two ($param1) {
-                function Test-Three ($param1) { }
+            function Test-Two ([int]$param1) {
+                function Test-Three ([bool]$param1, [string]$param2) { }
             }
 '@
     }
@@ -78,9 +78,9 @@ Describe "Get-FunctionName Tests" {
 
             # Assert
             $result | Should -Not -BeNullOrEmpty
-            $result | Should -Contain "Test-One"
-            $result | Should -Contain "Test-Two"
-            $result | Should -Not -Contain "Test-Three"
+            $result.Name | Should -Contain "Test-One"
+            $result.Name | Should -Contain "Test-Two"
+            $result.Name | Should -Not -Contain "Test-Three"
             $result.Count | Should -BeExactly 2
         }
 
@@ -89,10 +89,10 @@ Describe "Get-FunctionName Tests" {
             $result = Get-Item $tempPath | Get-FunctionName
 
             # Assert
-            $result | Should -Not -BeNullOrEmpty
-            $result | Should -Contain "Test-One"
-            $result | Should -Contain "Test-Two"
-            $result | Should -Not -Contain "Test-Three"
+            $result.Name | Should -Not -BeNullOrEmpty
+            $result.Name | Should -Contain "Test-One"
+            $result.Name | Should -Contain "Test-Two"
+            $result.Count | Should -BeExactly 2
         }
 
         It "Should accept IncludeNestedFunctions parameter" {
@@ -100,12 +100,57 @@ Describe "Get-FunctionName Tests" {
             $result = Get-FunctionName -Path $tempPath -IncludeNestedFunctions
 
             # Assert
-            $result | Should -Contain "Test-One"
-            $result | Should -Contain "Test-Two"
-            $result | Should -Contain "Test-Three"
+            $result.Name | Should -Contain "Test-One"
+            $result.Name | Should -Contain "Test-Two"
+            $result.Name | Should -Contain "Test-Three"
+            $result.Count | Should -BeExactly 3
         }
 
-        It "Should return an empty array if script has only comments and variables" {
+        It "Result from first (parent) function should have expected properties" {
+            # Act
+            $result = Get-FunctionName -Path $tempPath
+
+            # Assert
+            $result[0].Name | Should -BeExactly 'Test-One'
+            $result[0].IsFilter | Should -BeFalse
+            $result[0].Parameters | Should -BeNullOrEmpty
+            $result[0].LineNumber | Should -BeExactly 1
+            $result[0].FilePath | Should -Match 'TestScript.ps1$'
+            $result[0].FileName | Should -BeExactly 'TestScript.ps1'
+            $result[0].Text | Should -Match '^function Test-One'
+        }
+
+        It "Result from second (parent) function should have expected properties" {
+            # Act
+            $result = Get-FunctionName -Path $tempPath
+
+            # Assert
+            $result[1].Name | Should -BeExactly 'Test-Two'
+            $result[1].IsFilter | Should -BeFalse
+            $result[1].Parameters | Should -Not -BeNullOrEmpty
+            $result[1].Parameters[0].Name | Should -BeExactly '$param1'
+            $result[1].LineNumber | Should -BeExactly 2
+            $result[1].FilePath | Should -Match 'TestScript.ps1$'
+            $result[1].FileName | Should -BeExactly 'TestScript.ps1'
+            $result[1].Text | Should -Match '^function Test-Two'
+        }
+
+        It "Result from third (nested) function should have expected properties" {
+            # Act
+            $result = Get-FunctionName -Path $tempPath -IncludeNestedFunctions
+
+            # Assert
+            $result[2].Name | Should -BeExactly 'Test-Three'
+            $result[2].IsFilter | Should -BeFalse
+            $result[2].Parameters | Should -Not -BeNullOrEmpty
+            $result[2].Parameters[0].Name | Should -BeExactly '$param1'
+            $result[2].Parameters[1].Name | Should -BeExactly '$param2'
+            $result[2].LineNumber | Should -BeExactly 3
+            $result[2].FilePath | Should -Match 'TestScript.ps1$'
+            $result[2].FileName | Should -BeExactly 'TestScript.ps1'
+            $result[2].Text | Should -Match '^function Test-Three'
+        }
+        It "Should return an empty if script has only comments and variables" {
             # Arrange: Create a script with no functions in TestDrive
             $commentOnlyScriptPath = "TestDrive:\CommentOnlyScript.ps1"
             @"
@@ -115,7 +160,6 @@ Describe "Get-FunctionName Tests" {
 
             # Act
             $result = Get-FunctionName -Path $commentOnlyScriptPath
-
             # Assert
             $result | Should -BeNullOrEmpty
         }
--- Get-FunctionName.ps1
+++ Get-FunctionName.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.1.0.0
+.VERSION 1.2.0.0
 
 .GUID 3d43aa60-7d79-4ccf-8493-cabf4761f690
 
@@ -25,53 +25,53 @@
 .EXTERNALSCRIPTDEPENDENCIES
 
 .RELEASENOTES
-Extracts toplevel function names from PowerShell scripts using the AST for static analysis and tooling.
+Extracts function AST detail from PowerShell scripts using the AST for static analysis and tooling.
 
 .PRIVATEDATA
 
 .DESCRIPTION
 Parses a PowerShell script using the PowerShell Abstract Syntax Tree (AST)
-    and returns the names of the functions
+and returns function definition details, like the names and line number of the defined functions
 #>
 
 function Get-FunctionName {
     <#
-
-.DESCRIPTION
-    Parses a PowerShell script using the PowerShell Abstract Syntax Tree (AST)
-    and returns the names of all filter and function definitions that are not nested
-    inside other functions.
-    This is useful for static analysis, linting, documentation generation.
-.SYNOPSIS
-    Retrieves top-level function names from a PowerShell script file.
-.PARAMETER Path
-    Path to a PowerShell script file (.ps1, .psm1, etc.) to analyze.
-    The path must exist. This parameter accepts pipeline input.
-.EXAMPLE
-    Get-FunctionName -Path .\MyScript.ps1
-    Returns all top-level function names defined in MyScript.ps1.
-.EXAMPLE
-    Get-ChildItem *.psm1 | Get-FunctionName
-    Parses multiple module files and outputs their top-level function names.
-.OUTPUTS
-    System.String
-    The name of each top-level function found in the script.
-.NOTES
-    Author: Paul Naughton
-    Date: Jan 2026
-#>
+    .DESCRIPTION
+        Parses a PowerShell script using the PowerShell Abstract Syntax Tree (AST)
+        and returns the names of all filter and function definitions that are not nested
+        inside other functions.
+        This is useful for static analysis, linting, documentation generation.
+    .SYNOPSIS
+        Retrieves top-level function names from a PowerShell script file.
+    .PARAMETER Path
+        Path to a PowerShell script file (.ps1, .psm1, etc.) to analyze.
+        The path must exist. This parameter accepts pipeline input.
+    .EXAMPLE
+        Get-FunctionName -Path .\MyScript.ps1
+        Returns all top-level function names defined in MyScript.ps1.
+    .EXAMPLE
+        Get-ChildItem *.psm1 | Get-FunctionName
+        Parses multiple module files and outputs their top-level function names.
+    .OUTPUTS
+        System.String
+        The name of each top-level function found in the script.
+    .NOTES
+        Author: Paul Naughton
+        Date: Feb 2026
+    #>
 
     [CmdletBinding()]
     param (
         [Parameter(
             Mandatory,
             ValueFromPipeline,
-            ValueFromPipelineByPropertyName)]
+            ValueFromPipelineByPropertyName,
+            Position = 0)]
         [string]$Path,
 
-        # optionally search nested functions
+        # optionally include nested functions in search
         [Parameter()]
-        [switch]$IncludeNestedFunctions = $false
+        [switch]$IncludeNestedFunctions
     )
 
     Begin {
@@ -87,10 +87,18 @@ function Get-FunctionName {
         if ($ast -isnot [System.Management.Automation.Language.ScriptBlockAst]) { break }
 
         # extract FunctionDefinitionAst
-        $ast.FindAll( {
+        $FunctionDefinitionAst = $ast.FindAll( {
                 param($node)
-                $node -is [System.Management.Automation.Language.FunctionDefinitionAst] -and
-                ($IncludeNestedFunctions -or $node.Parent -isnot [System.Management.Automation.Language.FunctionDefinitionAst])
-            }, $IncludeNestedFunctions).Name
+                $node -is [System.Management.Automation.Language.FunctionDefinitionAst]
+            }, $IncludeNestedFunctions
+        )
+
+        # output a list of parameters with some caclulated parameters.
+        $FunctionDefinitionAst | Select-Object Name, IsFilter, Parameters,
+        @{N = 'LineNumber'; E = { $_.Extent.StartLineNumber } },
+        @{N = 'FilePath'; E = { $_.Extent.File } },
+        @{N = 'FileName'; E = { [System.IO.Path]::GetFileName($_.Extent.File) } },
+        @{N = 'Text'; E = { $_.Extent.Text } }
     }
+    End {}
 }
